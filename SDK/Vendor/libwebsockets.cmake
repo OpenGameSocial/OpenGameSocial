752ec7af8ba500a9a4b63ad5abb3d63889c4a24b
Revan600 <shiteater.jaja@gmail.com>
2025-07-09 19:32:40 +0400

    Fixed macos compilation Added platform configuration

--- SDK/Vendor/libwebsockets.cmake
+++ SDK/Vendor/libwebsockets.cmake
@@ -21,34 +21,38 @@ set(LWS_WITHOUT_SERVER ON CACHE BOOL "" FORCE) # don't build server part
 set(MBEDTLS_INCLUDE_DIRS "${mbedtls_SOURCE_DIR}/include" CACHE PATH "" FORCE)
 set(LWS_MBEDTLS_INCLUDE_DIRS "${mbedtls_SOURCE_DIR}/include" CACHE PATH "" FORCE)
 
-if(WIN32)
+if (WIN32)
     set(MBEDTLS_LIBRARY "${mbedtls_BINARY_DIR}/library/mbedtls.lib;bcrypt" CACHE FILEPATH "" FORCE)
     set(MBEDX509_LIBRARY "${mbedtls_BINARY_DIR}/library/mbedx509.lib" CACHE FILEPATH "" FORCE)
     set(MBEDCRYPTO_LIBRARY "${mbedtls_BINARY_DIR}/library/mbedcrypto.lib" CACHE FILEPATH "" FORCE)
-else()
+else ()
     set(MBEDTLS_LIBRARY "${mbedtls_BINARY_DIR}/library/libmbedtls.a" CACHE FILEPATH "" FORCE)
     set(MBEDX509_LIBRARY "${mbedtls_BINARY_DIR}/library/libmbedx509.a" CACHE FILEPATH "" FORCE)
     set(MBEDCRYPTO_LIBRARY "${mbedtls_BINARY_DIR}/library/libmbedcrypto.a" CACHE FILEPATH "" FORCE)
-endif()
+endif ()
 
 FetchContent_MakeAvailable(libwebsockets)
 
-#god, why LWS has so bad fetchcontent support?
-target_compile_definitions(websockets PUBLIC LWS_HAVE_MBEDTLS_AUTH_KEY_ID)
-target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_ssl_conf_alpn_protocols)
-target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_ssl_get_alpn_protocol)
-target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_ssl_conf_sni)
-target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_ssl_set_hs_ca_chain)
-target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_ssl_set_hs_own_cert)
-target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_ssl_set_hs_authmode)
-target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_net_init)
-target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_x509_crt_parse_file) # some embedded may lack filesystem
-target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_md_setup) # not on xenial 2.2
-target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_rsa_complete) # not on xenial 2.2
-target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_internal_aes_encrypt)
-target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_ssl_set_verify)
+if (NOT APPLE)
+    target_compile_definitions(websockets PUBLIC LWS_HAVE_MBEDTLS_AUTH_KEY_ID)
+    target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_ssl_conf_alpn_protocols)
+    target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_ssl_get_alpn_protocol)
+    target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_ssl_conf_sni)
+    target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_ssl_set_hs_ca_chain)
+    target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_ssl_set_hs_own_cert)
+    target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_ssl_set_hs_authmode)
+    target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_net_init)
+    target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_x509_crt_parse_file) # some embedded may lack filesystem
+    target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_md_setup) # not on xenial 2.2
+    target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_rsa_complete) # not on xenial 2.2
+    target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_internal_aes_encrypt)
+    target_compile_definitions(websockets PUBLIC LWS_HAVE_mbedtls_ssl_set_verify)
+endif ()
 
 set_property(TARGET websockets PROPERTY INTERFACE_INCLUDE_DIRECTORIES "")
+
+set(new_libs "")
+set_target_properties(websockets PROPERTIES INTERFACE_LINK_LIBRARIES "${new_libs}")
 
 target_link_libraries(SDK PUBLIC websockets)
 target_include_directories(SDK PUBLIC "${libwebsockets_BINARY_DIR}/include")
