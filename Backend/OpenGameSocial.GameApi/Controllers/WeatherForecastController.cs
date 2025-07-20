4f88aa119df7b56a2258b427a13c3f94928e8266
revan <shiteater.jaja@gmail.com>
2025-07-20 07:49:36 +0400

    Fixed test request on backend

--- Backend/OpenGameSocial.GameApi/Controllers/WeatherForecastController.cs
+++ Backend/OpenGameSocial.GameApi/Controllers/WeatherForecastController.cs
@@ -13,20 +13,28 @@ public class WeatherForecastController : ControllerBase
 
     private readonly ILogger<WeatherForecastController> _logger;
 
+    public class WeatherForecastResult
+    {
+        public WeatherForecast[] Data { get; init; }
+    }
+
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
     }
 
     [HttpPost(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Post([FromBody] WeatherRequestModel model)
+    public WeatherForecastResult Post([FromBody] WeatherRequestModel model)
     {
-        return Enumerable.Range(1, model.Count).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+        return new()
+        {
+            Data = Enumerable.Range(1, model.Count).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray()
+        };
     }
 }
