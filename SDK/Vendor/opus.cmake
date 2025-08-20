d73adb864cacd93e4dedd69fa20120f7abd6f64b
Skilloff <offskilloffical@gmail.com>
2025-08-20 22:34:26 +0400

    Added opus 1.5.2;

---
+++ SDK/Vendor/opus.cmake
@@ -0,0 +1,11 @@
+﻿include(FetchContent)
+
+FetchContent_Declare(
+        opus
+        GIT_REPOSITORY https://github.com/xiph/opus.git
+        GIT_TAG v1.5.2
+)
+
+FetchContent_MakeAvailable(opus)
+
+target_link_libraries(SDK PUBLIC opus)
